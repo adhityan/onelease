9346faf824d19fc44cf36b9d5fa87012058accb0
Adhityan <adhityan@hotmail.com>
2016-09-23 21:14:17 +0530

    refactor

--- header.php
+++ header.php
@@ -120,8 +120,8 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.html">
-                    <div class="logo"></div>
+                <a class="navbar-brand" href="/">
+                    <div style="margin-top: 11px;">Onelease</div>
                 </a>
             </div><!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
