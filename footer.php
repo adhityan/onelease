96c02d1d745b55be6eaaaa1d0f53eea72f3b7285
Adhityan <adhityan@hotmail.com>
2016-09-23 18:31:11 +0530

    working copy

---
+++ footer.php
@@ -0,0 +1,57 @@
+<footer class="content-info" role="contentinfo">
+    <div class="footer container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="pull-left">
+                    <div class="navbar">
+                        <ul class="nav spaces"><h4>About us</h4>
+                            <li class="menu-our-story"><a href="story/index.html">Our Story</a></li>
+                            <li class="menu-press"><a href="press/index.html">Press</a></li>
+                            <li class="menu-faq"><a href="faq/index.html">FAQ</a></li>
+                            <li class="menu-contact"><a href="contact/index.html">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="pull-left">
+                    <div class="navbar">
+                        <ul class="nav spaces"><h4>Our Spaces</h4>
+                            <li class="menu-berlin"><a href="/mumbai">Mumbai</a></li>
+                            <li class="menu-hamburg"><a href="/delhi">New Delhi</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="pull-left">
+                    <div class="navbar">
+                        <ul class="nav spaces"><h4>Our Projects</h4>
+                            <li class="menu-hardware-co"><a target="_blank" href="http://hardware.co">Hardware.co</a>
+                            </li>
+                            <li class="menu-betapitch"><a target="_blank" href="http://betapitch.net">betapitch</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="pull-left">
+                    <div class="navbar">
+                        <ul class="nav spaces"><h4>Social</h4>
+                            <li class="menu-facebook"><a href="http://www.facebook.com/betahaus">Facebook</a></li>
+                            <li class="menu-twitter"><a href="http://www.twitter.com/betahaus">Twitter</a></li>
+                            <li class="menu-instagram"><a href="https://www.instagram.com/betahaus/">Instagram</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12 belowtheline">
+                <hr>
+                <p class="pull-left">© 2016 Onelease.com</p>
+                <p class="pull-right">The Finest in Coworking in India</p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<script type="text/javascript" defer="" src="wp-content/cache/autoptimize/2/js/autoptimize_e91658977788d9289ec8028bf65c9465.js"></script>
+<img alt="preload image" style="display: none;">
+<img alt="preload image" style="display: none;">
+</body>
+</html>
